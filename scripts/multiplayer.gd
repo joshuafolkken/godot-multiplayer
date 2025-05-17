805e76dd965a33e9b5d0963b47049c1bdbf8d9c7
joshuafolkken <joshuafolkken@gmail.com>
2025-05-17 16:12:56 +0900

    chat #8

--- scripts/multiplayer.gd
+++ scripts/multiplayer.gd
@@ -24,3 +24,36 @@ func _on_host_button_pressed() -> void:
 func _on_join_button_pressed() -> void:
 	_peer.create_client(ADDRESS, PORT)
 	multiplayer.multiplayer_peer = _peer
+
+
+func _on_send_button_pressed() -> void:
+	var message_text_edit: TextEdit = $MessageTextEdit
+	var message := message_text_edit.text.strip_edges(true, true)
+	if message.is_empty():
+		return
+
+	message_text_edit.clear()
+	message_text_edit.grab_focus()
+
+	var my_player: Player = get_node(str(multiplayer.get_unique_id()))
+	if my_player:
+		my_player.show_chat_message(message)
+
+
+func _input_send(event: InputEvent) -> void:
+	var message_text_edit: TextEdit = $MessageTextEdit
+	if not message_text_edit.has_focus():
+		return
+
+	if not event is InputEventKey:
+		return
+
+	var key_event := event as InputEventKey
+
+	if key_event.keycode == KEY_ENTER:
+		get_viewport().set_input_as_handled()
+		_on_send_button_pressed()
+
+
+func _input(event: InputEvent) -> void:
+	_input_send(event)
